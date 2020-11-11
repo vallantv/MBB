d9189369f446686a914464c787118988f4fe874d
IKnewOne <41551327+IKnewOne@users.noreply.github.com>
2020-11-11 17:58:08 +0200

    HandyNotes Shadowlands Pin Ignore
    Fix for MBB - HandyNotes_Shadowlands bug regarding the Pin from the second addons not being ignored correctly by the MBB.  Further info
    MBB/issues/13

--- MBB.lua
+++ MBB.lua
@@ -100,7 +100,8 @@ MBB_Ignore = {
 	[41] = "PremadeFilter_MinimapButton",    -- PreMadeFilter
 	[42] = "GarrisonMinimapButton",
 	[43] = "TukuiMinimapZone",
-	[44] = "GPSArrow"
+	[44] = "GPSArrow",
+	[45] = "HandyNotes_ShadowlandsPin"
 };
 
 MBB_IgnoreSize = {
